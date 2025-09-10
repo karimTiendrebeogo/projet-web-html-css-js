7fb878444e30a5fa98b5e35aea8216c317dac201
karimTiendrebeogo <karimtdg73@gmail.com>
2025-09-10 19:02:47 +0000

    portfolio

--- script.js
+++ script.js
@@ -15,27 +15,27 @@ themeToggle.addEventListener('click', () => {
   icon.classList.toggle('fa-sun');
 });
 
-// Validation formulaire
-const form = document.getElementById('contactForm');
-const formMessage = document.getElementById('formMessage');
+// // Validation formulaire
+// const form = document.getElementById('contactForm');
+// const formMessage = document.getElementById('formMessage');
 
-form.addEventListener('submit', function(e){
-  e.preventDefault();
-  const nom = document.getElementById('nom').value.trim();
-  const email = document.getElementById('email').value.trim();
-  const message = document.getElementById('message').value.trim();
+// form.addEventListener('submit', function(e){
+//   e.preventDefault();
+//   const nom = document.getElementById('nom').value.trim();
+//   const email = document.getElementById('email').value.trim();
+//   const message = document.getElementById('message').value.trim();
 
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  if(nom === '' || !emailRegex.test(email) || message.length < 10){
-    formMessage.style.color = 'red';
-    formMessage.textContent = 'Veuillez remplir correctement tous les champs.';
-  } else {
-    formMessage.style.color = 'green';
-    formMessage.textContent = 'Merci ! Votre message a été envoyé.';
-    form.reset();
-  }
-});
+//   if(nom === '' || !emailRegex.test(email) || message.length < 10){
+//     formMessage.style.color = 'red';
+//     formMessage.textContent = 'Veuillez remplir correctement tous les champs.';
+//   } else {
+//     formMessage.style.color = 'green';
+//     formMessage.textContent = 'Merci ! Votre message a été envoyé.';
+//     form.reset();
+//   }
+// });
 
 // Bouton retour en haut
 const backToTop = document.getElementById('backToTop');
@@ -51,25 +51,3 @@ backToTop.addEventListener('click', () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 });
 
-
-// // // Menu mobile
-// // const menuToggle = document.getElementById("menu-toggle");
-// // const nav = document.getElementById("nav");
-
-// // menuToggle.addEventListener("click", () => {
-// //   nav.classList.toggle("show");
-// // });
-
-// // // Thème sombre / clair
-// // const themeToggle = document.getElementById("theme-toggle");
-// // const body = document.body;
-// // const themeIcon = themeToggle.querySelector("i");
-
-// // themeToggle.addEventListener("click", () => {
-// //   body.classList.toggle("dark");
-// //   if (body.classList.contains("dark")) {
-// //     themeIcon.classList.replace("fa-moon", "fa-sun");
-// //   } else {
-// //     themeIcon.classList.replace("fa-sun", "fa-moon");
-// //   }
-// // });
